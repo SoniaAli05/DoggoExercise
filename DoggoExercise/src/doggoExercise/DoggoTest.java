0d83d71241ef6ccd8d6baf9e85de1249bc6f026e
SoniaAli05 <45198070+SoniaAli05@users.noreply.github.com>
2018-12-07 15:37:50 +0000

    Add files via upload
    This exercise was to take a user input and return a list of 0-100 without the value that the user inputted displayed in that list. This is a maven project with a currently incomplete test.

---
+++ DoggoExercise/src/doggoExercise/DoggoTest.java
@@ -0,0 +1,13 @@
+package doggoExercise;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class DoggoTest {
+
+	@Test
+	public void test() {
+		int a = Placement;  
+		assertEquals("Included in list", 5, null);
+	}
+}
